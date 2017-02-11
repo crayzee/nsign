5abf0948823204170cdc01f0cf7ed118040ef509
crayzee <twopik@gmail.com>
2017-02-11 23:25:19 +0300

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,18 @@
 <?php
 return [
-		'language' => 'ru-RU', // <- здесь!
+    
+    'language' => 'ru-RU', // <- здесь!
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=eco24_1',
+            'username' => 'eco24',
+            'password' => 'Metalloff90909012',
+            'charset' => 'utf8',
         ],
     ],
 		
